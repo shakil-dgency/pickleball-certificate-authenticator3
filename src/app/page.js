81f9fc6e0b5b7d7822b003aff2e6f4909385a6e0
shakil <shakil.dgency@gmail.com>
2025-02-26 11:53:22 +0600

    deployment test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import GlobalContainer from "@/components/GlobalContainer";
+
 
 export default function page() {
 	
 	return (
-		<div className="h-[100v] pt-">
-			<GlobalContainer />
-			{/* <h1 className="text-black">Hello from home page</h1> */}
+		<div className="h-[100vh] pt-20">
+			<h1 className="text-black">Hello from home page</h1>
 		</div>
 	);
 }
