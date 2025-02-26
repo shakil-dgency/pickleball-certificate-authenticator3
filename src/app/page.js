fc3ce2460fa650b886daeddce6b2b86021163bd9
shakil <shakil.dgency@gmail.com>
2025-02-26 12:02:18 +0600

    create home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+import AboutAndGallery from "@/components/Home/AboutAndGallery";
+import Hero from "@/components/Home/Hero/Hero";
+import PurchaseIncludes from "@/components/Home/PurchaseIncludes";
 
 
 export default function page() {
 	
 	return (
-		<div className="h-[100vh] pt-20">
-			<h1 className="text-black">Hello from home page</h1>
+		<div className="">
+			<Hero />
+			<AboutAndGallery />
+			<PurchaseIncludes />
 		</div>
 	);
 }
